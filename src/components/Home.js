99e71dd0a6ffa48fa11dc31af9543b13d24e7842
Nipa <akternipa941@gmail.com>
2022-07-01 23:21:45 +0600

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+
+    const [tasks, setTasks] = useState([]);
 
     const handleTask = event => {
 
@@ -10,6 +12,7 @@ const Home = () => {
         const item = {
             task: task,
         }
+
 
         const url = 'http://localhost:5000/task';
         fetch(url, {
@@ -29,22 +32,39 @@ const Home = () => {
         event.target.reset();
     }
 
+
+
+    useEffect(() => {
+        const url = 'http://localhost:5000/task';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setTasks(data);
+            })
+    }
+        , [tasks])
+
     return (
         <div className='bg-violet-300  py-12 '>
             <div className='text-center'>
                 <h1 className='text-violet-900 text-3xl'>Add Your task Here</h1> <br />
-                <form onClick={handleTask}>
-                <input type="text" placeholder="Type here and press enter button" name='task' class="input input-bordered input-primary w-full max-w-xs" />
+                <form onSubmit={handleTask}>
+                    <input type="text" placeholder="Type here and press enter button" name='task' class="input input-bordered input-primary w-full max-w-xs" />
+                    <input type="submit" value="Submit" />
                 </form>
-           
+
             </div>
 
             <div className='w-1/2 mx-auto p-6 border my-12'>
-                 <p className='text-violet-900 text-2xl'>To-do List</p> 
-                <hr />
-                <li></li>
-            </div>
+            <p className='text-violet-900 text-2xl'>To-do List</p>     <hr />
+                {
+                    tasks.map(i=> <li key={i._id}>{i.task}</li>)
+                }
+               
            
+            
+            </div>
+
         </div>
     );
 };
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ToDo = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const url = 'http://localhost:5000/task';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setTasks(data);
+            })
+
+    }
+        , [])
+
+
+
     return (
         <div className='bg-violet-300 py-16'>
-            <h1 className='text-center text-violet-900 text-3xl pb-8'>Completed Task</h1>
+            <h1 className='text-center text-violet-900 text-3xl pb-8'>Added Task</h1>
             <div className='border w-2/5 mx-auto p-12'>
                 <div class="form-control">
-                    <label class="label cursor-pointer">
-                        <input type="checkbox" class="checkbox checkbox-primary" />
-                        <span class="label-text">Remember me</span>
+                    <label class="">
+
+                        {
+                            tasks.map(item => <div>
+                                <div className='flex items-center my-4'>
+                                    <input type="checkbox" class="checkbox checkbox-primary mr-6" />
+                                    <span key={item._id} class="label-text">{item.task}</span>
+                                </div>
+                            </div>
+                            )
+                        }
+
                     </label>
                 </div>
             </div>
